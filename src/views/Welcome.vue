f44183726aa860e10132305acfc8e99c91112184
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2021-03-14 14:20:42 -0300

    chore: clean up

---
+++ src/views/Welcome.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="welcome container">
+    <h1>Welcome</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+  components: {}
+};
+</script>
+
+<style>
+.welcome {
+  padding: 20px 0;
+  text-align: center;
+}
+</style>
