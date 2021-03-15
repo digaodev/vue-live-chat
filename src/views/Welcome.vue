a29918d475c5a38481a709f2128bea419d61b3a7
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2021-03-15 15:27:07 -0300

    feat: Add Welcome page

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,19 +1,48 @@
 <template>
   <div class="welcome container">
     <h1>Welcome</h1>
+    <SignupForm />
   </div>
 </template>
 
 <script>
+import SignupForm from '../components/SignupForm.vue'
+
 export default {
   name: "Welcome",
-  components: {}
+  components: { SignupForm }
 };
 </script>
 
 <style>
-.welcome {
-  padding: 20px 0;
-  text-align: center;
-}
+ .welcome {
+    text-align: center;
+    padding: 20px 0;
+  }
+  /* form styles */
+  .welcome form {
+    width: 300px;
+    margin: 20px auto;
+  }
+  .welcome label {
+    display: block;
+    margin: 20px 0 10px;
+  }
+  .welcome input {
+    width: 100%;
+    padding: 10px;
+    border-radius: 20px;
+    border: 1px solid #eee;
+    outline: none;
+    color: #999;
+    margin: 10px auto;
+  }
+  .welcome span{
+    font-weight: bold;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+  .welcome button {
+    margin: 20px auto;
+  }
 </style>
