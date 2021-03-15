bdba14a030e1519a92bde8fa6e41266ad557e33e
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2021-03-15 15:22:06 -0300

    fix: import global css file

--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
 import { createApp } from "vue";
+
 import App from "./App.vue";
 import router from "./router";
+
+// import global css
+import './assets/main.css'
 
 createApp(App)
   .use(router)
