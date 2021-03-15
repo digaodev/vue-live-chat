52e306894fe3a38211a78f6d80219d4a7a214534
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2021-03-15 15:26:38 -0300

    feat: add Signup page

---
+++ src/components/SignupForm.vue
@@ -0,0 +1,54 @@
+<template>
+  <form @submit.prevent="handleSubmit">
+    <input
+      type="text"
+      id="displayName"
+      name="displayName"
+      placeholder="displayName"
+      required
+      v-model="displayName"
+    />
+
+    <input
+      type="email"
+      id="email"
+      name="email"
+      placeholder="email"
+      required
+      v-model="email"
+    />
+
+    <input
+      type="password"
+      id="password"
+      name="password"
+      placeholder="password"
+      required
+      v-model="password"
+    />
+
+    <button>Sign Up</button>
+  </form>
+</template>
+
+<script>
+import { ref } from "vue";
+
+export default {
+  name: "SignupForm",
+  setup() {
+    const displayName = ref("");
+    const email = ref("");
+    const password = ref("");
+
+    function handleSubmit() {
+      console.log(displayName.value, email.value, password.value);
+    }
+
+    return { displayName, email, password, handleSubmit };
+  },
+};
+</script>
+
+<style>
+</style>
