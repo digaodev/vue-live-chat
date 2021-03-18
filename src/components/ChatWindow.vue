f8dc7969173a2a651510749312208f58ee8afb44
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2021-03-18 16:32:07 -0300

    feat: add ChatWindow component

---
+++ src/components/ChatWindow.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="chat-window">
+    <div v-if="error">{{ error }}</div>
+    <ul class="messages" v-if="documents">
+      <li class="single" v-for="doc in documents" :key="doc.id">
+        <span class="created-at">{{ doc.createdAt.toDate() }}</span>
+        <span class="name">{{ doc.user }}</span>
+        <span class="message">{{ doc.message }}</span>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import getCollection from "../composables/getCollection";
+
+export default {
+  setup() {
+    const { error, documents } = getCollection("messages");
+
+    return { error, documents };
+  },
+};
+</script>
+
+<style scoped>
+.chat-window {
+  background: #fafafa;
+  padding: 30px 20px;
+  text-align: left;
+}
+.chat-window ul {
+  list-style-type: none;
+}
+.single {
+  margin: 18px 0;
+}
+.created-at {
+  display: block;
+  
+  color: #999;
+  font-size: 12px;
+  margin-bottom: 4px;
+}
+.name {
+  font-weight: bold;
+  margin-right: 6px;
+}
+.messages {
+  max-height: 400px;
+  overflow: auto;
+}
+</style>
