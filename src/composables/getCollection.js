42513beff37281df512bd70f19b2102ee0537a3c
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2021-03-18 16:30:35 -0300

    feat: add getCollection composable

---
+++ src/composables/getCollection.js
@@ -0,0 +1,29 @@
+import { ref } from 'vue'
+
+import { db } from '../services/firebase'
+
+function getCollection(collectionName) {
+  const error = ref(null)
+  const documents = ref(null)
+
+  let collectionRef = db.collection(collectionName).orderBy('createdAt')
+
+  collectionRef.onSnapshot(snap => {
+    let results = []
+
+    snap.docs.forEach(doc => {
+      doc.data().createdAt && results.push({ ...doc.data(), id: doc.id })
+    })
+
+    documents.value = results
+    error.value = null
+  }, (err) => {
+    console.log(err.message)
+    documents.value = null
+    error.value = 'Oops! Could not fetch any data'
+  })
+
+  return { error, documents }
+}
+
+export default getCollection
