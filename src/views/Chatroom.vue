1e01fd131c3e76febaa3bc4b5f807eafe444cc82
Rodrigo RB <rodrigo.rb.dev@gmail.com>
2021-03-18 16:32:47 -0300

    feat: render ChatWindow and NewChatForm components on the Chatroom

--- src/views/Chatroom.vue
+++ src/views/Chatroom.vue
@@ -1,15 +1,19 @@
 <template>
   <div class="container">
     <NavBar />
+    <ChatWindow />
+    <NewChatForm />
   </div>
 </template>
 
 <script>
 import NavBar from "../components/NavBar.vue";
+import NewChatForm from "../components/NewChatForm.vue";
+import ChatWindow from "../components/ChatWindow.vue";
 
 export default {
   name: "Chatroom",
-  components: { NavBar },
+  components: { NavBar, NewChatForm, ChatWindow },
 };
 </script>
 
